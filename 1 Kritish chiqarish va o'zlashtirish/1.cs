e8fd5dbe65b9a993f8b556d9366f775614597e3e
Islomjon <turgunovislomjon107@gmail.com>
2026-05-12 04:45:41 +0500

    updated question

--- 1 Kritish chiqarish va o'zlashtirish/1.cs
+++ 1 Kritish chiqarish va o'zlashtirish/1.cs
@@ -1,1 +1,161 @@
-Console.WriteLine("Hello, World!");
+using system;
+// Masala 1
+
+Console.Write("A = ");
+double a = double.Parse(Console.ReadLine());
+double P = 4 * a;
+Console.WriteLine("P = " + P);
+   
+// Masala 2
+
+
+
+// Masala 3
+
+
+
+// Masala 4
+
+
+
+// Masala 5
+
+
+
+// Masala 6
+
+
+
+// Masala 7
+
+
+
+// Masala 8
+
+
+
+// Masala 9
+
+
+
+// Masala 10
+
+
+
+// Masala 11
+
+
+
+// Masala 12
+
+
+
+// Masala 13
+
+
+
+// Masala 14
+
+
+
+// Masala 15
+
+
+
+// Masala 16
+
+
+
+// Masala 17
+
+
+
+// Masala 18
+
+
+
+// Masala 19
+
+
+
+// Masala 20
+
+
+
+// Masala 21
+
+
+
+// Masala 22
+
+
+
+// Masala 23
+
+
+
+// Masala 24
+
+
+
+// Masala 25
+
+
+
+// Masala 26
+
+
+
+// Masala 27
+
+
+
+// Masala 28
+
+
+
+// Masala 29
+
+
+
+// Masala 30
+
+
+
+// Masala 31
+
+
+
+// Masala 32
+
+
+
+// Masala 33
+
+
+
+// Masala 34
+
+
+
+// Masala 35
+
+
+
+// Masala 36
+
+
+
+// Masala 37
+
+
+
+// Masala 38
+
+
+
+// Masala 39
+
+
+
+// Masala 40
