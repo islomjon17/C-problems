6ee079ed794701533a30a51458efb26fcd8b16d4
Islomjon <turgunovislomjon107@gmail.com>
2026-05-16 22:21:16 +0500

    updated

--- 1 Kritish chiqarish va o'zlashtirish/1.cs
+++ 1 Kritish chiqarish va o'zlashtirish/1.cs
@@ -273,7 +273,20 @@ using system;
 
 // Masala 22
 
+// Console.Write("A nuqtani kiriting: ");
+// double A = double.Parse(Console.ReadLine());
+// Console.Write("B nuqtani kiriting: ");
+// double B = double.Parse(Console.ReadLine());
+// Console.Write("C nuqtani kiriting: ");
+// double C = double.Parse(Console.ReadLine());
 
+// double AC = C - A;
+// double BC = C - B;
+// double yigindi = AC + BC;
+
+// Console.WriteLine("AC = " + AC);
+// Console.WriteLine("BC = " + BC);
+// Console.WriteLine("Yig'indi = " + yigindi);
 
 // Masala 23
 
